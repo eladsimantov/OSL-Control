66da6146520af06cdd5681e0dc9c358b98ab28c9
Elad Siman Tov <62391381+eladsimantov@users.noreply.github.com>
2025-12-23 14:08:38 +0200

    feat: build rubric for architecture of arik example

---
+++ examples/run_arik_example.py
@@ -0,0 +1,16 @@
+"""
+This example will activate the knee and ankle joints by position 
+commands using the our custom drivers library. 
+It will not use the CLI, only a 1 minute example of 
+activation, calibration, and positions.
+"""
+from src.drivers.ODriveCAN import ODriveMotor
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
+
