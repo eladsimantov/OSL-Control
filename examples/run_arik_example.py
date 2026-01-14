15466f8bf968fb3f351658dca860a95fd84492ee
David-Linux <ddavidovitch@campus.technion.ac.il>
2026-01-14 17:52:01 +0200

    i added some tasks and started working on torque control

--- examples/run_arik_example.py
+++ examples/run_arik_example.py
@@ -30,25 +30,32 @@ os.system("sudo ip link set can0 up type can bitrate 250000")
 def main():
     #initiation 
     can1 = ODriveCAN(node_id=1)
-    knee = ODriveMotor(can1, name="knee")
+    knee = ODriveMotor(can1, name="knee",gear_ratio=40)
     can2 = ODriveCAN(node_id=2)
     ankle = ODriveMotor(can2, name="ankle")
     
-    # # calibration - make new funciton to reset_position to zero 
-    # ankle.calibrate(5) # for 5 seconds..
-    # knee.calibrate(5)
+    # calibration - make new funciton to reset_position to zero 
+    ankle.calibrate(5) # for 5 seconds..
+    knee.calibrate(5)
+    #there is a problem with the calibration
 
-    # Statte 3 - closed loop control
-    ankle.closed_loop()
-    knee.closed_loop()
-    time.sleep(3)
 
-    #i dont understand the calibration process arik wrote 
+    #happens outimatically in calibrate function!
+    #ankle.closed_loop()
+    #knee.closed_loop()
+    #time.sleep(3)
+
+    
     #set state 3 - calibrate the motor (internal things, not the home angle)
     #home deg - what we use to tell the position we want. i need to check if it is absolute or relative to the turning on 
     #according to gemini the endcoder is encramental- so we define the home deg, maybe easier to change the code so the 0 changes 
     # why fo we need idle in the calibration?
 
+#if we use sleep,the leg need time to reach the position. the code runs before it.
+
+    #first movement
+    knee.position_deg(0)
+    time.sleep(10)
     #oriention 
     degKnee = 10
     #need to check for float or integar 
@@ -57,11 +64,30 @@ def main():
     ankle.position_deg(degAnkle)
     delay = 3 # seconds
     time.sleep(delay)
+    knee.position_deg(90)
+    time.sleep(30)
     degKnee = 0
     knee.position_deg(degKnee)
+    time.sleep(30)
     degAnkle = 0
     ankle.position_deg(degAnkle)
+    time.sleep(delay)
 
+    #trajectory
+
+
+    #velocity
+
+
+    #torque - for this parameter i got an error  
+    #knee.torque_nm(3)
+    #time.sleep(5)
+    #knee.torque_nm(0)
+    #safety 
+    
+
+    #2 motors opration simultaneously
+    
 #finishing the example
     ankle.idle()
     knee.idle()
