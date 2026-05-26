6d2d042df8a9be3a25fbd5b7c5b7ad6d5b2223d9
Elad_RPI_osl <eladsimantov@gmail.com>
2026-05-26 12:52:06 +0300

    fix: make terminal kill move knee to idle via keyboardinterrupt

--- tests/test_actuator.py
+++ tests/test_actuator.py
@@ -108,14 +108,27 @@ def test_sine_movement():
     knee.position_deg(0)
     knee.set_state(1)
 
+def test_idle():
+    dbc_path="/home/enable-lab/Desktop/OSL-Control/src/drivers/odrive-cansimple.dbc"
+    can1 = ODriveCAN(node_id=0,dbc_path=dbc_path)
+    knee = ODriveMotor(can1, name="knee", gear_ratio=40)
+    knee.idle()
+
+
 if __name__ == "__main__":
     CAN_CH = 'can0'
     BITRATE = 1000000  # Default is 1Mb/s 
     # os.system(f"source /home/enable-lab/Desktop/OSL-Control/.venv/bin/activate")
     os.system(f"sudo ip link set {CAN_CH} down")
     os.system(f"sudo ip link set {CAN_CH} up type can bitrate {BITRATE}")
-    # test_position_control()
-    # test_velocity_control()
-    # test_torque_control(0.2)
-    # test_sine_movement()
-    test_impedance_control()
+    try:
+        # test_position_control()
+        # test_velocity_control()
+        # test_torque_control(0.2)
+        # test_sine_movement()
+        test_impedance_control()
+    
+    except KeyboardInterrupt:
+        print("Moving Knee to Idle mode")
+        test_idle()    
+        os.system(f"sudo ip link set {CAN_CH} down")
